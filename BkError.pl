8530d435ea14c8e6a66cb56aa1cb84a823d177cc
Brian Lindauer <lindauer@counterstorm.com>
2003-06-12 01:15:09 +0000

    Convert to Log::Dispatch for error logging.

--- BkError.pl
+++ BkError.pl
@@ -1,16 +1,50 @@
+use Log::Dispatch;
+use Log::Dispatch::Syslog;
+use Log::Dispatch::Screen;
+
 package BkError;
+use strict;
 {
-  my $debug;
-
-
-
-  sub new($$)
+  sub new()
   {
-    ($this, $debug) = @_;
+    my ($class, $ident, $print_level, $log_level, $log_method, $debug) = @_;
+
+    die "Internal error: missing required parameters for BkError.\n" unless ($ident && $print_level && $log_level);
+
     my $self = {};
     bless $self;
-    print STDERR "Debugging on ($debug).\n" if ($debug);
-    $debug = 0 unless ($debug);
+
+    $self->{'debug'} = $debug || 0;
+
+    if ($log_method)
+    {
+      if (($log_method ne 'inet') && ($log_method ne 'unix'))
+      {
+	die "Invalid syslog method ($log_method).  Must be 'inet' or 'unix'.\n";
+      }
+    }
+    else
+    {
+      $log_method = 'inet';
+    }
+    
+    die "Invalid print level ($print_level).\n" if !Log::Dispatch->level_is_valid($print_level);
+    die "Invalid log level ($log_level).\n" if !Log::Dispatch->level_is_valid($log_level);
+
+    my $logger = Log::Dispatch->new();
+    
+    $logger->add( Log::Dispatch::Syslog->new( name => 'syslogger',
+					      min_level => $log_level,
+					      ident => $ident,
+					      logopt => 'pid',
+					      facility => 'user',
+					      socket => $log_method ));
+    
+    $logger->add( Log::Dispatch::Screen->new( name => 'screenlogger',
+					      min_level => $print_level ));
+    
+    $self->{'logger'} = $logger;
+
     return $self;
   }
 
@@ -20,21 +54,40 @@ package BkError;
   # msg - debug message to print
   # level - Minimum debug level to print message (default 1)
   #
-  sub dprint($$)
+  sub dprint($$$)
   {
-    my ($this, $msg, $level) = @_;
+    my ($self, $msg, $level) = @_;
+    my ($errlevel);
+    my ($debug) = $self->{'debug'};
 
-    $level = 1 if (!defined($level));
+    $level = 1 unless ($level);
 
-    print STDERR $_[1] if ($debug >= $level);
+    if ($debug >= $level)
+    {
+      err_print($self, $msg, 'debug');
+    }
   }
 
 
 
-  sub err_print($)
+  sub err_print($$$)
   {
-    print STDERR $_[1];
+    my ($self, $message, $level) = @_;
+    my $print_level = $self->{'print_level'};
+    my $logger = $self->{'logger'};
+
+    $level = 'err' unless ($level);
+
+    if (!Log::Dispatch->level_is_valid($level))
+    {
+      $logger->log(level => 'err', message => "Internal error: invalid log level ($level).\n");
+      $level = 'err';
+    }
+
+    $logger->log(level => $level, message => $message);
   }
+
+    
 }
 
 return 1;
