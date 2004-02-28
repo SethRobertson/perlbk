7379b86b2c68ff42110208cb9dcab7d01cfd822e
James Tanis <jtt@counterstorm.com>
2004-02-28 00:44:55 +0000

    Lots of common install things. Still more to do.

--- Baka/Conf.pm
+++ Baka/Conf.pm
@@ -1,4 +1,5 @@
-# $Id: Conf.pm,v 1.1 2003/12/31 16:56:18 lindauer Exp $
+# -*- perl -*-
+# $Id: Conf.pm,v 1.2 2004/02/28 00:44:52 jtt Exp $
 #
 # ++Copyright LIBBK++
 # 
@@ -17,13 +18,11 @@
 # @file
 # Read bk.conf format file
 #
-
-
 package Baka::Conf;
 require Exporter;
 use FileHandle;
 @ISA = qw (Exporter);
-@EXPORT_OK = qw(get get_sections get_keys);
+@EXPORT_OK = qw(get get_sections get_keys set_uniq_value);
 $VERSION = 1.00;
 {
   sub new($)
@@ -73,6 +72,7 @@ $VERSION = 1.00;
   }
 
 
+
   # Lookup the value of a key.  Section is global unless specified.
   sub get($$$)
   {
@@ -105,6 +105,38 @@ $VERSION = 1.00;
     return undef unless ($sec_hash);
 
     return keys(%{$sec_hash});
+  }
+
+  
+  # Set a value in a simple baka conf with no sections and unique values
+  # Return 1 on success, 0 on failure.
+  sub set_uniq_value($$$)
+  {
+    my($self, $key, $value) = @_;
+    my(@lines);
+    my($found_key) = 0;
+    my($ret) = 1;
+
+    # Update myself
+    $self{'sections'}{'global'}{$key} = $value;
+    
+    # Update the file. First read and replace the value if the key is found.
+    open(CONF_IN, "< $self->{'filename'}") || die "Failed to open $self->{'filename'} for reading: $!.\n";
+    @lines = grep(s/^\s*$key\s*=.*/$key = $value/ && ($found_key = 1) || 1, <CONF_IN>);
+    close(CONF_IN);
+
+    # Add the key/value pair if the key was not found
+    push @lines, "$key = $value\n" if (!$found_key);
+
+    # Write out new file.
+    open(CONF_OUT, "> $self->{'filename'}+") || 
+      die "Failed to open $self->{'filename'} for writing: $!.\n";
+
+    print CONF_OUT "" . join ('', @lines);
+
+    close(CONF_OUT);
+    rename("$self->{'filename'}+", "$self->{'filename'}");
+    return($ret);
   }
 };
 
