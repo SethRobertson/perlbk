2ef7c6b907cc1c78bccf4215f8ea1b31d17bf4f8
Alex Dupuy <dupuy@counterstorm.com>
2004-06-23 19:11:11 +0000

    INSIDER-1_0-rollup-4

---
+++ perl/BkReportGen/helper_linuxstats.pl
@@ -0,0 +1,56 @@
+######################################################################
+#
+# ++Copyright LIBBK++
+#
+# Copyright (c) $YEAR The Authors. All rights reserved.
+#
+# This source code is licensed to you under the terms of the file
+# LICENSE.TXT in this release for further details.
+#
+# Mail <projectbaka\@baka.org> for further information
+#
+# --Copyright LIBBK--
+#
+# A standard reports helper
+#
+# <description>Display certain statistics available on linux.  No errors available.</description>
+
+sub helper_linuxstats($$$$)
+{
+  my ($Inforef, $Storedref, $Outputarrayref, $Opt) = @_;
+  my ($data) = scalar(`vmstat`);
+  my ($Outref);
+  my ($old) = $/;
+
+  if ($? != 0)
+  {
+    return "vmstat command failed: $?\n";
+  }
+
+  undef($Outref);
+  $Outref->{'name'} = "vmstat";
+  $Outref->{'data'} = $data;
+  push(@$Outputarrayref, $Outref);
+
+  undef($/);
+
+  foreach my $f ("/proc/stat", "/proc/meminfo", "/proc/net/softnet_stat", "/proc/net/sockstat", "/proc/net/netstat", "/proc/slabinfo")
+  {
+    if (-f $f)
+    {
+      open(F,$f) || next;
+      $data = <F>;
+      close(F);
+
+      undef($Outref);
+      $Outref->{'name'} = "$f (for expert diagnostics)";
+      $Outref->{'data'} = $data;
+      push(@$Outputarrayref, $Outref);
+    }
+  }
+
+  $/ = $old;
+  1;
+}
+
+1;
